7114a9e2a65450f13d9496750eb5fbea0a600ac7
Devon Campbell <raddevon@gmail.com>
2013-09-03 08:42:52 -0400

    Adds permission decorators

--- flask_permissions/core.py
+++ flask_permissions/core.py
@@ -9,4 +9,45 @@ class Permissions(object):
         global db
         db = local_db
 
+    def user_has(attribute):
+        """
+        Takes an attribute (a string name of either a role or an ability) and returns the function if the user has that attribute
+        """
+        def wrapper(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                attribute_object = Role.query.filter_by(name=attribute).first()
+                user_abilities = []
+                for role in current_user.roles:
+                    user_abilities += [
+                        role.ability for ability in role.abilities]
+                if attribute_object in current_user.roles or attribute_object in user_abilities:
+                    return func(*args, **kwargs)
+                else:
+                    # Make this do someting way better.
+                    return "You do not have access"
+            return inner
+        return wrapper
+
+    def user_is(attribute):
+        """
+        Takes an attribute (a string name of either a role or an ability) and returns the function if the user has that attribute
+        """
+        def wrapper(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                attribute_object = Ability.query.filter_by(
+                    name=attribute).first()
+                user_abilities = []
+                for role in current_user.roles:
+                    user_abilities += [
+                        role.ability for ability in role.abilities]
+                if attribute_object in current_user.roles or attribute_object in user_abilities:
+                    return func(*args, **kwargs)
+                else:
+                    # Make this do someting way better.
+                    return "You do not have access"
+            return inner
+        return wrapper
+
 import models
