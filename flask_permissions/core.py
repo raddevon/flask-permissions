372ff72dded21cee12e1f1950e60a3873b96c6b8
Devon Campbell <raddevon@gmail.com>
2013-09-13 10:06:07 -0400

    Fixes decorator logic

--- flask_permissions/core.py
+++ flask_permissions/core.py
@@ -19,13 +19,14 @@ class Permissions(object):
         def wrapper(func):
             @wraps(func)
             def inner(*args, **kwargs):
-                from .models import Role
-                attribute_object = Role.query.filter_by(name=attribute).first()
+                from .models import Ability
+                desired_ability = Ability.query.filter_by(
+                    name=attribute).first()
                 user_abilities = []
                 for role in self.current_user.roles:
                     user_abilities += [
                         role.ability for ability in role.abilities]
-                if attribute_object in current_user.roles or attribute_object in user_abilities:
+                if desired_ability in user_abilities:
                     return func(*args, **kwargs)
                 else:
                     # Make this do someting way better.
@@ -40,12 +41,9 @@ class Permissions(object):
         def wrapper(func):
             @wraps(func)
             def inner(*args, **kwargs):
-                from .models import Ability
-                attribute_object = Ability.query.filter_by(
+                from .models import Role
+                desired_role = Role.query.filter_by(
                     name=attribute).first()
-                user_abilities = []
-                    user_abilities += [
-                        role.ability for ability in role.abilities]
                 if desired_role in self.current_user.roles:
                     return func(*args, **kwargs)
                 else:
