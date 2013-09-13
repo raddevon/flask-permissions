76ff4336f9e1b48565a53013c08649473c388e43
Devon Campbell <raddevon@gmail.com>
2013-09-13 10:00:00 -0400

    Initial commit

---
+++ flask_permissions/tests.py
@@ -0,0 +1,12 @@
+from flask.ext.testing import TestCase
+from .core import Permissions
+from .models import Role, Ability, UserMixin
+
+
+class ModelsTests(TestCase):
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        return app
