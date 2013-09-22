863c556603dfa48d741fd7d115d7bb1e738ab764
Devon Campbell <raddevon@gmail.com>
2013-09-22 12:32:40 -0400

    Updates version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Permissions',
-    version='0.1.15',
+    version='0.1.16',
     url='http://example.com/flask-sqlite3/',
     license='BSD',
     author='Devon Campbell',
